fd8221080d899afd0238ae13a7f4f959fb0b4ec9
jnfrick <156953679+jnfrick@users.noreply.github.com>
2024-03-12 13:09:55 -0700

    Update build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,10 +4,18 @@ set -ex
 
 # /build/busybox wget https://meganode.wallarm.com/4.6/wallarm-4.6.0.x86_64-glibc.tar.gz -O - | tar -xzv -C /
 # /build/busybox wget https://meganode.wallarm.com/4.8/wallarm-4.8.4.x86-glibc.tar.gz -O - | tar -xzv -C /
+# /build/busybox wget https://meganode.wallarm.com/4.8/wallarm-4.8.8.x86_64-glibc.sh 
+# sh ./wallarm-4.8.8.x86_64-glibc.sh --target /opt/wallarm --keep --noexec
 
-/build/busybox wget https://meganode.wallarm.com/4.8/wallarm-4.8.8.x86_64-glibc.sh 
-sh ./wallarm-4.8.8.x86_64-glibc.sh --target /opt/wallarm --keep --noexec
+/build/busybox wget https://meganode.wallarm.com/4.10/wallarm-4.10.2.x86_64-glibc.sh
+sh ./wallarm-4.10.2.x86_64-glibc.sh --target /opt/wallarm --keep --noexec
+
 cd /opt/wallarm/modules/
+
+# Kong 3.1.1 (only use one or the other)
+# ln -s ./kong-12141 kong
+
+# Kong 3.4.2
 ln -s ./kong-342-openresty-12141-jammy kong
 
 chown -R kong:kong /opt/wallarm
